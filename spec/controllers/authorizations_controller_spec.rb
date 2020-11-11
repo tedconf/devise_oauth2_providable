99db3b3ff416f8ce6414547c5a92e87bc7b66d7c
Michael Twentyman <20@thirdfloorstudios.com>
2020-11-11 15:44:47 -0500

    update controller specs to use let vs with

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Devise::Oauth2Providable::AuthorizationsController, type: :controller do
+  let(:client) { FactoryBot.create(:client) }
+  let(:user) { FactoryBot.create(:user) }
   routes { Devise::Oauth2Providable::Engine.routes }
   describe 'GET #new' do
     context 'with valid redirect_uri' do
-      with :user
-      with :client
       let(:redirect_uri) { client.redirect_uri }
       before do
         sign_in user
-        get :new, :client_id => client.identifier, :redirect_uri => redirect_uri, :response_type => 'code'
+        get :new, params: { client_id: client.identifier, redirect_uri: redirect_uri, response_type: 'code' }
       end
       it { is_expected.to respond_with :ok }
       # it { is_expected.to respond_with_content_type :html }
@@ -25,12 +25,10 @@ describe Devise::Oauth2Providable::AuthorizationsController, type: :controller d
       it { is_expected.to render_with_layout 'application' }
     end
     context 'with invalid redirect_uri' do
-      with :user
-      with :client
       let(:redirect_uri) { 'http://example.com/foo/bar' }
       before do
         sign_in user
-        get :new, :client_id => client.identifier, :redirect_uri => redirect_uri, :response_type => 'code'
+        get :new, params: { client_id: client.identifier, redirect_uri: redirect_uri, response_type: 'code' }
       end
       it { is_expected.to respond_with :bad_request }
       it 'should have the right content_type' do
--- spec/controllers/protected_controller_spec.rb
+++ spec/controllers/protected_controller_spec.rb
@@ -1,40 +1,45 @@
 require 'spec_helper'
 
 describe ProtectedController, type: :controller do
+  let(:client) { FactoryBot.create(:client) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:refresh_token) { client.refresh_tokens.create! user: user }
+  let(:access_token) {
+    Devise::Oauth2Providable::AccessToken.create!(
+      client: client, user: user, refresh_token: refresh_token
+    )
+  }
 
   describe 'get :index' do
-    with :client
-    with :user
-    before do
-      @token = Devise::Oauth2Providable::AccessToken.create! :client => client, :user => user
-    end
     context 'with valid bearer token in header' do
       before do
-        @request.env['HTTP_AUTHORIZATION'] = "Bearer #{@token.token}"
-        get :index, :format => 'json'
+        @request.env['HTTP_AUTHORIZATION'] = "Bearer #{access_token.token}"
+        get :index, as: :json
       end
       it { is_expected.to respond_with :ok }
     end
+
     context 'with valid bearer token in query string' do
       before do
-        get :index, :access_token => @token.token, :format => 'json'
+        get :index, params: { access_token: access_token.token }, as: :json
       end
       it { is_expected.to respond_with :ok }
     end
 
     context 'with invalid bearer token in query param' do
       before do
-        get :index, :access_token => 'invalid', :format => 'json'
+        get :index, params: { access_token: 'invalid' }, as: :json
       end
       it { is_expected.to respond_with :unauthorized }
     end
+
     context 'with valid bearer token in header and query string' do
       before do
       end
       it 'raises error' do
         expect {
-          @request.env['HTTP_AUTHORIZATION'] = "Bearer #{@token.token}"
-          get :index, :access_token => @token.token, :format => 'json'
+          @request.env['HTTP_AUTHORIZATION'] = "Bearer #{access_token.token}"
+          get :index, params: { access_token: access_token.token }, as: :json
         }.to raise_error(Exception)
       end
     end
