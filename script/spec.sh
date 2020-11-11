561a8f3b94690776da92fdd9caaacab26ae49e45
Michael Twentyman <20@thirdfloorstudios.com>
2020-11-11 15:45:58 -0500

    run most recent rails 1st

--- script/spec.sh
+++ script/spec.sh
@@ -2,11 +2,11 @@ base_dir=`dirname $(dirname "$0")`
 ruby_version=`cat $base_dir/.ruby-version | tr -d '\n'`
 ruby_versions='2.5.7 2.6.5 2.7.0'
 ruby_gemset=`cat $base_dir/.ruby-gemset | tr -d '\n'`
-for i in `ls $base_dir/gemfiles/Gemfile* | grep -v lock`
+for i in `ls $base_dir/gemfiles/Gemfile* | grep -v lock | sort -r`
 do
   for rb in $ruby_versions
   do
     export BUNDLE_GEMFILE=$i
-    rvm "$rb@$ruby_gemset" exec ruby --version && bundle update && rails --version && rake spec
+    rvm "$rb@$ruby_gemset" --create exec ruby --version && bundle update && rails --version && rake spec
   done
 done
